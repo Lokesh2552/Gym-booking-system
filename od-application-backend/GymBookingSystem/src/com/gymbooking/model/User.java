4a83c6a2d646f6dc962fa7ede606d4665477e425
Blog User <blog@example.com>
2026-01-15 14:45:47 +0100

    Add project files

---
+++ od-application-backend/GymBookingSystem/src/com/gymbooking/model/User.java
@@ -0,0 +1,48 @@
+package com.gymbooking.model;
+
+public class User {
+    private int id;
+    private String username;
+    private String email;
+    private String fullName;
+
+    // Constructors, getters, setters
+    public User(int id, String username, String email, String fullName) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.fullName = fullName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
